367aa345ee1b47f525595b5e623269026f8bb16d
bebejan01 <mustafademirel047@gmail.com>
2026-03-15 15:42:49 +0300

    Added

---
+++ Cpp_examples/Hafta_01/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+class Dikdortgen {
+	private:
+		int uzunluk;
+		int genislik=5;
+		
+	public:
+		int genislik2 = 2;
+		
+		
+		
+};
+
+
+
+
+int main(int argc, char** argv) {
+	/*
+	int a = 5;
+	int b, c;
+	
+	std::cout<<"Hello World"<<std::endl;
+	std::cout<<a<<std::endl;
+	std::cout<<"1 sayiyi giriniz: ";
+	std::cin>>b;
+	std::cout<<"2 sayiyi giriniz: ";
+	std::cin>>c;
+	
+	int toplam = b+c;
+	std::cout<<"b: "<<b<<" c: "<<c<<" Toplam: "<<toplam;  */
+	
+	
+	Dikdortgen dortgen;
+	
+	std::cout<< dortgen.genislik2;
+	
+	
+	
+	
+	
+	
+	return 0;
+}
